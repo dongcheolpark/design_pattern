c0fde398d7bcee2a6a3a5fe29781081683101150
dongcheolpark <head02325@gmail.com>
2021-08-14 16:11:51 +0900

    singleton pattern

--- src/singleton.cpp
+++ src/singleton.cpp
@@ -6,14 +6,17 @@ using namespace std;
 class Settings {
 private:
 	Settings() {}
-	static Settings * set;
 	string text = "hello,world!";
 public:
 	static Settings * getSettings() {
-		if(set == NULL) {
-			set = new Settings();
-		}
-		return set;
+		static Settings * instance = new Settings();
+		return instance;
+	}
+	void setSettings(string str) {
+		text = str;
+	}
+	void print_text() {
+		cout<<text<<endl;
 	}
 };
 
@@ -22,9 +25,29 @@ private:
 	Settings * set = NULL;
 public:
 	firstpage() {
+		set = Settings::getSettings();
+	}
+	void change_set_text() {
+		set->print_text();
+		set->setSettings("change text");
+	}
+};
+
+class secondpage {
+private:
+	Settings * set = NULL;
+public:
+	secondpage() {
+		set = Settings::getSettings();
+	}
+	void print() {
+		set->print_text();
 	}
 };
 
 int main() {
-
+	firstpage a;
+	secondpage b;
+	a.change_set_text();
+	b.print();
 }
